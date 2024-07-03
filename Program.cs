b31b9e8eddbb0e2b17926b9ee289d846bf2142b3
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-07-03 19:15:33 -0300

    array em c#.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DotnetLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5]; //pode usar o int[] no lugar do var caso queira
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
+
+        }
+    }
+}
