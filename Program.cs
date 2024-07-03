ba6b92ff8f1a43cae539db000847571f8111eb60
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-07-03 19:18:12 -0300

    push no c#

--- Program.cs
+++ Program.cs
@@ -8,12 +8,9 @@ namespace DotnetLists
             Console.Clear();
 
             var meuArray = new int[5]; //pode usar o int[] no lugar do var caso queira
+            //Console.WriteLine(meuArray[0]);
+            meuArray[0] = 12;
             Console.WriteLine(meuArray[0]);
-            Console.WriteLine(meuArray[1]);
-            Console.WriteLine(meuArray[2]);
-            Console.WriteLine(meuArray[3]);
-            Console.WriteLine(meuArray[4]);
-
         }
     }
 }
